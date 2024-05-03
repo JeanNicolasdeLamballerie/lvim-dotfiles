24be8113ca8330fa9f3ddba7d0f45169181b52b2
JeanNicolasdeLamballerie <55099344+JeanNicolasdeLamballerie@users.noreply.github.com>
2024-05-03 08:47:03 +0200

    config

--- config.lua
+++ config.lua
@@ -145,3 +145,19 @@ require 'lspconfig'.sqlls.setup {
     return vim.loop.cwd()
   end,
 }
+local colors = require('midnight.colors')
+local p = colors.palette    -- raw color palette
+local c = colors.components -- component color powershell
+
+require('midnight').setup {
+  HighlightGroup = {
+    fg = ForegroundColor, -- :h guifg
+    bg = BackgroundColor, -- :h guibg
+    sp = SpecialColor,    -- :h guisp
+    style = RenderStyle,  -- :h attr-list
+    -- OR
+    --   link = TargetHiglightGroup     -- :h :hi-link (link to "TargetHiglightGroup")
+    -- OR
+    -- clear = true                   -- :h :hi-clear (clear "HighlightGroup"; `false` ignores this option)
+  },
+}
